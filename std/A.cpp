4d1297d2d5bb496c9cfbb920b8610d4b0c04a8d3
AgOH <50107074+StableAgOH@users.noreply.github.com>
2022-04-25 14:20:39 +0800

    Initial commit

---
+++ std/A.cpp
@@ -0,0 +1,9 @@
+#include <iostream>     // 头文件
+using namespace std;    // 使用 std 命名空间
+int main()              // 主函数
+{
+    int a,b;            // 两个变量 a,b
+    cin>>a>>b;          // 读入
+    cout<<a+b<<endl;    // 输出
+    return 0;           // 返回 0 代表正常退出
+}
